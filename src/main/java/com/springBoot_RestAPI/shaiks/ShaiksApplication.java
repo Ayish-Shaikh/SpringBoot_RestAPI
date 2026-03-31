d2cb9f936123137b5616d6505958ab4803f9bf43
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-03-31 22:23:39 +0530

    setting up a spring boot based project

---
+++ src/main/java/com/springBoot_RestAPI/shaiks/ShaiksApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot_RestAPI.shaiks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaiksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShaiksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springBoot_RestAPI/shaiks/ShaiksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot_RestAPI.shaiks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaiksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
