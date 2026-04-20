623ee3752f0dbb2fcc078ed290ae72747238323b
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-20 22:03:04 +0530

    this file is used for converting the message into json structure

--- src/main/java/com/springBoot_RestAPI/shaiks/HelloResponse.java
+++ src/main/java/com/springBoot_RestAPI/shaiks/HelloResponse.java
@@ -1,4 +1,18 @@
 package com.springBoot_RestAPI.shaiks;
 
 public class HelloResponse {
+
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
